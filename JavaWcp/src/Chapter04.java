53c894a70d4f8004fb4c3ef7440a1d020aac18ac
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-07-19 20:43:15 +0900

    [Finish] Java練習4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String nm;
+		nm = "Okitaka Ishii";
+		System.out.println(nm);
+		
+		nm = "為せば成る";
+		System.out.println(nm);
+		
+		final int LUCKY_NUMBER = 777;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ JavaWcp/src/Variable01.java
@@ -0,0 +1,17 @@
+
+public class Variable01 {
+	public static void main(String[] args) {
+		int num1;//int型の変数num1の宣言
+		String str1;//String型の変数str1の宣言
+		num1 = 10;//num1に初期値を代入
+		str1 = "Hellow World!";//str1に初期値を代入
+		System.out.println(num1);
+		System.out.println(str1);
+		
+		num1 = 20;//num1に再代入
+		System.out.println(num1);
+		
+		int num2 = 30;//int型の変数num2の宣言及びnum2に初期値を代入
+		System.out.println(num2);
+	}
+}
