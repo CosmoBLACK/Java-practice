d6d5089f993a49bdf5b3a07cbd555d0fc26ed03f
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-07-20 23:46:13 +0900

    [Finish] Java練習5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long a = 10000000000L;
+		System.out.println(a);
+		double b = 3.14;
+		System.out.println(b);
+		boolean c = true;
+		System.out.println(c);
+		
+		Integer d = 777;
+		System.out.println(d);
+		Float e = 1.4142f;
+		System.out.println(e);
+		Character f = 'あ';
+		System.out.println(f);
+		
+		int[] array = { 0, 10, 20, 30, 40 };
+		System.out.println(array[3]);
+	}
+}
