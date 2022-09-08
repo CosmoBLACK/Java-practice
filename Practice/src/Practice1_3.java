6483d604d61754ccee0e3fc926ccaf3faf586168
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-09-08 13:37:33 +0900

    [Finish] Practice2

--- Practice/src/Practice1_3.java
+++ Practice/src/Practice1_3.java
@@ -1,4 +1,10 @@
 
 public class Practice1_3 {
-
+	public static void main(String[] args) {
+		boolean result = true;
+		char favoriteCharacter = '駆';
+		double pi = 3.14;
+		long number = 314159265853979L;
+		String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+	}
 }
