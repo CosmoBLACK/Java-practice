c388652cd797ad1b63ab259e6cf7d304fbaf1c30
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-09-12 22:07:27 +0900

    [Finish] Practice5_1

---
+++ Practice/src/Practice5_1.java
@@ -0,0 +1,13 @@
+
+public class Practice5_1 {
+	public static void introduceOneself() {
+		String name = "サイトウ";
+		int age = 25;
+		double height = 170.5;
+		char zodiac = '子';
+		System.out.println("私の名前は" + name + "で、年齢は" + age + "歳で、身長は" + height + "cmで、十二支は" + zodiac + "です。");
+	}
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+}
