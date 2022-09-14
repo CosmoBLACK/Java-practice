50b0ac08db65f20a6d2d01fc2fceae9fce19c31f
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-09-14 20:06:50 +0900

    [Finish] Practice5_3

---
+++ Practice/src/Practice5_3.java
@@ -0,0 +1,20 @@
+
+public class Practice5_3 {
+	public static void email(String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：無題");
+		System.out.println("本文：" + text);
+	}
+	
+	public static void email(String title, String address, String text) {
+		System.out.println(address + "に、以下のメールを送信しました");
+		System.out.println("件名：" + title);
+		System.out.println("本文：" + text);
+	}
+	
+	public static void main(String[] args) {
+		String address = "test@test";
+		String text = "週末にお出かけしませんか？";
+		email(address, text);
+	}
+}
