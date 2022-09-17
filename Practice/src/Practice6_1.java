009600d590bd6a29b37562c18d52be0fe40b8a05
CosmoBLACK <101558828+CosmoBLACK@users.noreply.github.com>
2022-09-17 22:03:45 +0900

    [Finish] Practice6_1

--- Practice/src/Practice6_1.java
+++ Practice/src/Practice6_1.java
@@ -1,4 +1,12 @@
+import comment.Practice6_1_Zenhan;
 
 public class Practice6_1 {
-
+	public static void main(String[] args) throws Exception {
+		Practice6_1_Zenhan.doWarusa();
+		Practice6_1_Zenhan.doTogame();
+		comment.Practice6_1_Kouhan.callDeae();
+		comment.Practice6_1_Kouhan.showMondokoro();
+	}
+	
+	
 }
---
+++ Practice/src/comment/Practice6_1_Kouhan.java
@@ -0,0 +1,12 @@
+package comment;
+
+public class Practice6_1_Kouhan {
+	public static void callDeae() {
+		System.out.println("えぇい、こしゃくな。くせ者だ！であえい！");
+	}
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん、もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Practice6_1_Zenhan.doTogame();
+	}
+}
